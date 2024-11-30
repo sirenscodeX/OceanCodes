0d3520d3e98fc34f86d5f4e162bbd3bd7ac77930
sirenscodeX <malakiking47@gmail.com>
2024-11-30 15:50:24 -0500

    Add files via upload

---
+++ state tax assignment.py
@@ -0,0 +1,15 @@
+state_tax= 0.0625
+federal_tax=0.350
+dependent_deducation= 62.5
+salary= 1250.0
+dependents= 2
+state_witholding= 78.125
+federal_witholding= 437.5
+total_dependent_deducation= 125.0
+total_witholding= 390.625
+total_home_pay= 859.375
+print("State Tax: $", round(78.13, 2))
+print("Federal Tax: $", round(437.50, 2))
+print("Dependent Deducation: $", round(125.002))
+print("Total WItholding:$", round(390.63, 2))
+print("Take-Home Pay: $", round(859.38, 2))
